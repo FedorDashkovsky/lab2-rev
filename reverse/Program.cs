e4cee838d2287340492ff846527999662f6f989e
FedorDashkovsky <fedor.dashkovsky@gmail.com>
2021-04-10 02:40:45 +0300

    Add project files.

---
+++ reverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string list = " ну ка мечи стаканы на стол";
+            string[] reverse = list.Split(' ');
+            
+                for (int i = 6; i > 0; i--)
+            {
+
+                Console.WriteLine(reverse[i]);
+            }
+
+
+            Console.ReadKey();
+
+      
+
+            
+        }
+    }
+}
